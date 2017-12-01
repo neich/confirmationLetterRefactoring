7f96af30af3047a4e4102bec13c3e1799949d6b2
Ignacio Martín <ignacio.martin@udg.edu>
2017-12-01 12:03:01 +0100

    Add missing implementation

--- src/main/java/com/example/confirmationletter/domain/BatchTotal.java
+++ src/main/java/com/example/confirmationletter/domain/BatchTotal.java
@@ -3,8 +3,10 @@ package com.example.confirmationletter.domain;
 import java.math.BigDecimal;
 
 public class BatchTotal {
-  private BigDecimal creditValue;
-  private BigDecimal creditCounterValueForDebit;
+  private BigDecimal creditValue = BigDecimal.ZERO;
+  private BigDecimal creditCounterValueForDebit = BigDecimal.ZERO;
+  private BigDecimal debitValue = BigDecimal.ZERO;
+  private String transactionSign;
 
   public BigDecimal getCreditValue() {
     return creditValue;
@@ -20,5 +22,21 @@ public class BatchTotal {
 
   public void setCreditCounterValueForDebit(BigDecimal creditCounterValueForDebit) {
     this.creditCounterValueForDebit = creditCounterValueForDebit;
+  }
+
+  public void setDebitValue(BigDecimal debitValue) {
+    this.debitValue = debitValue;
+  }
+
+  public BigDecimal getDebitValue() {
+    return debitValue;
+  }
+
+  public void setTransactionSign(String transactionSign) {
+    this.transactionSign = transactionSign;
+  }
+
+  public String getTransactionSign() {
+    return transactionSign;
   }
 }
