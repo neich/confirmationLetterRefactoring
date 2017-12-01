958d1d1f759645231b55119b28257285af3a85ba
Ignacio Martín <ignacio.martin@udg.edu>
2017-12-01 12:03:23 +0100

    RF12.2: Refactor test

--- src/test/java/com/example/confirmationletter/ConfirmationLetterGeneratorTest.java
+++ src/test/java/com/example/confirmationletter/ConfirmationLetterGeneratorTest.java
@@ -1,6 +1,8 @@
 package com.example.confirmationletter;
 
 import com.example.confirmationletter.domain.BatchTotal;
+import com.example.confirmationletter.record.service.impl.Constants;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -10,19 +12,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConfirmationLetterGeneratorTest {
 
+  ConfirmationLetterGenerator letterGenerator;
+
+  @BeforeEach
+  void setup() {
+    letterGenerator = new ConfirmationLetterGenerator();
+  }
+
   @Test
-  public void testCreditBatchTotal_divider_one_all_credit() {
-    BatchTotal one = new BatchTotal();
-    one.setCreditValue(BigDecimal.ONE);
-    BatchTotal ten = new BatchTotal();
-    ten.setCreditValue(BigDecimal.TEN);
-
+  public void testCreditBatchTotal_divider_one_mixed_credit_debit() {
     HashMap<Integer, BatchTotal> batchTotals = new HashMap<Integer, BatchTotal>();
-    batchTotals.put(99, one);
-    batchTotals.put(98, ten);
+    batchTotals.put(99, createBatchTotal(1, Constants.CREDIT));
+    batchTotals.put(98, createBatchTotal(10, Constants.CREDIT));
+    batchTotals.put(97, createBatchTotal(5, Constants.DEBIT));
 
-    ConfirmationLetterGenerator letterGenerator = new ConfirmationLetterGenerator();
-    BigDecimal result = letterGenerator.creditBatchTotal(batchTotals, 1);
-    assertEquals(new BigDecimal(11), result);
+    assertEquals(new BigDecimal(11), letterGenerator.creditBatchTotal(batchTotals, 1));
+  }
+
+  private BatchTotal createBatchTotal(int number, String sign) {
+    BatchTotal bt = new BatchTotal();
+    bt.setTransactionSign(sign);
+    if (Constants.CREDIT.equals(sign)) {
+      bt.setCreditValue(new BigDecimal(number));
+    } else if (Constants.DEBIT.equals(sign)) {
+      bt.setDebitValue(new BigDecimal(number));
+    }
+    return bt;
   }
 }
