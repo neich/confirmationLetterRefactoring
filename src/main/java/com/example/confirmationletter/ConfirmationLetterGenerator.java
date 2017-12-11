3939f7f91cbdc0c48184b24e6838611058545241
Ignacio Martín <ignacio.martin@udg.edu>
2017-12-11 13:16:35 +0100

    RF25: created getCurrencyCode method, should be replaced by enum version/conversion (or Currency should do this type of conversion using a private enum)

--- src/main/java/com/example/confirmationletter/ConfirmationLetterGenerator.java
+++ src/main/java/com/example/confirmationletter/ConfirmationLetterGenerator.java
@@ -602,4 +602,17 @@ public class ConfirmationLetterGenerator {
     this.letterSelector = letterSelector;
   }
 
+  protected String getCurrencyByCode(Integer code) {
+
+    if (Constants.EUR_CURRENCY_CODE.equals(code)) {
+      return Constants.CURRENCY_EURO;
+    } else if (Constants.FL_CURRENCY_CODE.equals(code)
+        || Constants.FL_CURRENCY_CODE_FOR_WEIRD_BANK.equals(code)) {
+      return Constants.CURRENCY_FL;
+    } else if (Constants.USD_CURRENCY_CODE.equals(code)) {
+      return Constants.CURRENCY_USD;
+    } else {
+      throw new IllegalArgumentException("Unknown currency code encountered");
+    }
+  }
 }
